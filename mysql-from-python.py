d62614bc605851abf2b7d9306624fb8a33f9de76
antonrasegard <arasegard@gmail.com>
2020-08-12 10:03:45 +0000

    Added python code to be able to delete names from new list

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 username = os.getenv('C9_USER')
@@ -11,9 +12,10 @@ connection = pymysql.connect(host='localhost',
 
 try:
     with connection.cursor() as cursor:
-        sql = "select * from Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        list_of_names = ['Fred', 'Bob']
+        format_strings = ','.join(['%s']*len(list_of_names))
+        cursor.execute("delete from Friends where name in ({});".format(
+            format_strings), list_of_names)
+        connection.commit()
 finally:
     connection.close()
